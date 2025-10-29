47e4b1a8729e2c74293af03b93da0bdb19f7080d
SaarHurvitz <saar.hurvitz@post.runi.ac.il>
2025-10-29 23:19:12 +0200

    Updated Homework

--- Bill3.java
+++ Bill3.java
@@ -7,7 +7,7 @@ public class Bill3 {
 	    //Divides the bill value to 3 and rounds up
 	    bill = (int) Math.ceil((double)bill/3); 
 	    //output
-	    System.out.println(args[2] + ", " + args[1] + ", and " + args[0] + 
+	    System.out.println("Dear" + args[2] + ", " + args[1] + ", and " + args[0] + 
 		                   ": pay " + (double)bill + " Shekels each.");
 						   //BTW Iknow how to make a scanner but the assignment said to use args
 
